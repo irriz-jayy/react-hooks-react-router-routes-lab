6f7810897fc4b2d8e52f0c398f0c8a5abca7ee01
irriz-jayy <jayfelix789@gmail.com>
2023-01-21 11:50:49 +0300

    First commit. Managed to set navlink. Show H1 to pages

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const listDirectors = directors.map((director, index) => {
+    <li key={index}>{director}</li>;
+    console.log(director, index);
+  });
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      <ul>{listDirectors}</ul>
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,15 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const listMovie = movies.map((movie, index) => {
+    <li key={index}>{movie}</li>;
+  });
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <ul>{listMovie}</ul>
+    </div>
+  );
 }
 
 export default Movies;
