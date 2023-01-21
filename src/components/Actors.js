09ab56fb54262d7a6412b665a3fb38e4f15b3116
irriz-jayy <jayfelix789@gmail.com>
2023-01-21 11:55:08 +0300

    Done actors page. Onto debugging

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const listActor = actors.map((actor, index) => {
+    <li key={index}>{actor}</li>;
+    console.log(index, actor);
+  });
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <ul>test</ul>
+    </div>
+  );
 }
 
 export default Actors;
