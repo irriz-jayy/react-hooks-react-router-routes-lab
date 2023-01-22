92807a246bc7e1feb5ed454d672ae6747baf50f2
irriz-jayy <jayfelix789@gmail.com>
2023-01-22 09:52:15 +0300

    Completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,13 +2,16 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  const listMovie = movies.map((movie, index) => {
-    <li key={index}>{movie}</li>;
-  });
   return (
     <div>
       <h1>Movies Page</h1>
-      <ul>{listMovie}</ul>
+      {movies.map((movie, index) => (
+        <div className="movie" key={index}>
+          {movie.title}
+          {movie.time}
+          <li>{movie.genres}</li>
+        </div>
+      ))}
     </div>
   );
 }
