a0b163b34cb798ad601c72b2935ad66139c3ec82
irriz-jayy <jayfelix789@gmail.com>
2023-01-22 09:44:27 +0300

    Completed actors page

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,14 +2,13 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  console.log(actors);
   return (
     <div>
       <h1>Actors Page</h1>
       {actors.map((actor, index) => (
         <div className="actor" key={index}>
           {actor.name}
-          <ul>{actor.movies}</ul>
+          <li>{actor.movies}</li>
         </div>
       ))}
     </div>
