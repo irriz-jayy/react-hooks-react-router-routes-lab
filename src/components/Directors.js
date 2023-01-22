ee6e685cc8f4e210a35980f3e9c70edcdcef3210
irriz-jayy <jayfelix789@gmail.com>
2023-01-22 09:48:40 +0300

    completed directors

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,14 +2,15 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  const listDirectors = directors.map((director, index) => {
-    <li key={index}>{director}</li>;
-    console.log(director, index);
-  });
   return (
     <div>
       <h1>Directors Page</h1>
-      <ul>{listDirectors}</ul>
+      {directors.map((director, index) => (
+        <div className="director" key={index}>
+          {director.name}
+          <li>{director.movies}</li>
+        </div>
+      ))}
     </div>
   );
 }
